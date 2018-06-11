b35172afa0de2de625fe9d37b5a323ece9166754
lihao <lihao3016@gmail.com>
2018-06-11 21:11:25 +0800

    净身

---
+++ doc/gps/ATGM332D 北斗定位模块/例程参考/6.51单片机STC89C52例程/其它供参考例程/GPS信息获取代码（LCD12864带字库液晶并行模式）V1.7/main.h
@@ -0,0 +1,26 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+#include <reg52.h>
+
+////LED IO设置
+//sbit LED1 = P2^0;
+//sbit LED2 = P2^1;
+//#define LED_ON 0
+//#define LED_OFF 1
+
+//按键 IO设置
+sbit KEY4 = P3^6;
+
+
+//函数或者变量声明
+extern void Delay_ms(unsigned int n);
+extern void Scan_Key();
+extern void UTCDate2LocalDate(void);
+extern bit IsLeapYear(unsigned int uiYear);	
+extern unsigned char GetMaxDay(unsigned char Month_Value,unsigned int Year_Value);
+
+
+
+
+#endif
